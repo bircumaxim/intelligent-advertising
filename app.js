ee062ea60b2a85eb237e39556c3dcb9859afb141
Nicu Maxian <maxiannicu@gmail.com>
2017-11-11 04:20:24 +0200

    Removeed camera

--- app.js
+++
@@ -1,9 +0,0 @@
-var camera = require("./camera");
-var detection = require("./detection");
-var test = new camera();
-
-setInterval(() => {
-    test.takePhoto(() => {
-        detection("picture.jpg", data => console.log(data))
-    });
-}, 100);
