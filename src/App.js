e34663e5d8398cc341b5b048f58aa2907e24aa3e
Nicu Maxian <maxiannicu@gmail.com>
2017-11-11 02:46:56 +0200

    Refactor

--- src/App.js
+++ src/App.js
@@ -3,7 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
+
+    constructor() {
+        super();
+        console.log("huinea");
+    }
+
+    render() {
     return (
       <div className="App">
         <header className="App-header">
